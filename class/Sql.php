397f2a8131d72df346f61672059a9d60e87647d2
Paulo Roberto Borges <paulorborges@yahoo.com.br>
2019-04-20 15:56:43 -0300

    Criação da classe usuário

--- class/Sql.php
+++ class/Sql.php
@@ -1,12 +1,12 @@
 <?php
 	class Sql extends PDO{
 		private $conn;
-		private function setParam($statment, $param, $value){
-			$statment->bindParam($key,$value);
+		private function setParam($statement, $key, $value){
+			$statement->bindParam($key,$value);
 		}
-		private function setParams($statment, $parameters = array()){
+		private function setParams($statement, $parameters = array()){
 			foreach ($parameters as $key => $value){
-				$this -> setParam($key,$value);
+				$this -> setParam($statement, $key, $value);
 			}
 		}
 
--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 	require_once("config.php");
+	/* teste inicial com um select simples apenas para verificação do funcionamento das classes */
+	/*
 	$sql = new Sql();
 	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 	echo json_encode($usuarios);
+	*/
+	/* sequencia da construção do código para análise mais apurada das classes porém ainda com a visão de seleção de apenas um usuário */
+	$root = new Usuario();
+	$root->loadById(3);
+	echo $root;
 ?>
